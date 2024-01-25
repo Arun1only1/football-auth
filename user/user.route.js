6e8e82568a0037a182578a7d5dd7d0d920ebdd88
Arun1only1 <arun1nly1@gmail.com>
2024-01-25 16:10:06 +0545

    register and login

--- user/user.route.js
+++ user/user.route.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { userSchema } from "./user.validation.js";
+import { loginUserSchema, userRegisterSchema } from "./user.validation.js";
 import User from "./user.model.js";
 import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -14,7 +15,7 @@ router.post(
 
     try {
       // validate new user
-      const validatedData = await userSchema.validate(newUser);
+      const validatedData = await userRegisterSchema.validate(newUser);
 
       req.body = validatedData;
 
@@ -53,6 +54,63 @@ router.post(
     return res
       .status(201)
       .send({ message: "User is registered successfully." });
+  }
+);
+
+// login
+
+router.post(
+  "/user/login",
+  async (req, res, next) => {
+    // extract login credentials from req.body
+    const loginCredentials = req.body;
+    // validate
+    try {
+      const validatedData = await loginUserSchema.validate(loginCredentials);
+
+      req.body = validatedData;
+
+      // call next function
+      next();
+    } catch (error) {
+      // throw error
+
+      return res.status(400).send({ message: error.message });
+    }
+  },
+  async (req, res) => {
+    // extract login credentials from req.body
+    const loginCredentials = req.body;
+
+    // find user by email
+    const user = await User.findOne({ email: loginCredentials.email });
+
+    // if not user, throw error
+    if (!user) {
+      return res.status(404).send({ message: "Invalid credentials." });
+    }
+
+    // check for password match(bcrypt)
+    const isPasswordMatch = await bcrypt.compare(
+      loginCredentials.password,
+      user.password
+    );
+
+    // if not password match, throw error
+    if (!isPasswordMatch) {
+      return res.status(404).send({ message: "Invalid credentials." });
+    }
+
+    // generate token
+    const token = jwt.sign({ email: user.email }, "abc@3456");
+
+    // hide password from user
+    user.password = undefined;
+    // send response
+
+    return res
+      .status(200)
+      .send({ message: "logged in", token: token, user: user });
   }
 );
 export default router;
--- user/user.validation.js
+++ user/user.validation.js
@@ -1,6 +1,6 @@
 import Yup from "yup";
 
-export let userSchema = Yup.object({
+export let userRegisterSchema = Yup.object({
   name: Yup.string()
     .required("Name is required.")
     .trim()
@@ -18,4 +18,13 @@ export let userSchema = Yup.object({
     .trim()
     .min(4, "Password must be at least 4 characters.")
     .max(20, "Password must be at max 20 characters."),
+});
+
+export let loginUserSchema = Yup.object({
+  email: Yup.string()
+    .email("Must be valid email.")
+    .required("Email is required.")
+    .trim()
+    .lowercase(),
+  password: Yup.string().required("Password is required.").trim(),
 });
